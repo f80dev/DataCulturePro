658a149dfd5fdc18344a906d513b2f5a711077f1
f80dev <herve.hoareau@f80.fr>
2023-09-04 12:16:01 +0200

    commit from portable

---
+++ OpenAlumni/passwords.py
@@ -0,0 +1,13 @@
+#DB_USER="femis" #user pour l'hébergement via akash
+
+DB_USER="hhoareau" #user pour l'hébergement via serveur dolibarr
+DB_PASSWORD="hh4271" #<- mot de passe pour l'herbergement serveur
+#DB_PASSWORD="hh42714280" #mot de passe pour l'herbergement via akash network sur provider.europlots.com
+
+GIPHY_API_KEY="EL1SNvjG7dJNOC4r8Dwz7XKPf5I2uG5f"
+UNSPLASH_API_KEY="jULgaq6gGPDmyKi24nJO1Ta5SgdVpT38PjGBiu4snu4"
+
+EMAIL_HOST_PASSWORD ="UJybl921"
+
+_SECRET_KEY = ')0p@7b_kew8_w+jjuv=(zbn!sp!bm2*=7$s7#%@bvkwy0i--0p'
+RESET_PASSWORD="oui"
